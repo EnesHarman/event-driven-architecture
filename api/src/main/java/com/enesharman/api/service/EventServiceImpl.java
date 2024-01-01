bd448e5e497d97d1b34b0567a06d3507028f6cc5
enesharman <enesharman45@gmail.com>
2024-01-01 22:12:27 +0300

    Implement kafka producer for the api module

--- api/src/main/java/com/enesharman/api/service/EventServiceImpl.java
+++ api/src/main/java/com/enesharman/api/service/EventServiceImpl.java
@@ -1,13 +1,29 @@
 package com.enesharman.api.service;
 
 import com.enesharman.api.dto.EventFireRequest;
+import com.enesharman.constant.Topics;
+import com.enesharman.model.Event;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
-public class EventServiceImpl implements EventService{
+public class EventServiceImpl implements EventService {
+
+    private final KafkaTemplate<String, Event> producer;
+
+    public EventServiceImpl(KafkaTemplate<String, Event> producer) {
+        this.producer = producer;
+    }
 
     @Override
     public void fireEvent(EventFireRequest request) {
+        var event = populateEventFromRequest(request);
+        producer.send(Topics.EVENT_TOPIC, event);
+    }
 
+    private Event populateEventFromRequest(EventFireRequest request) {
+        return Event.builder()
+                .code(request.code())
+                .build();
     }
 }
---
+++ core/src/main/java/com/enesharman/constant/Topics.java
@@ -0,0 +1,5 @@
+package com.enesharman.constant;
+
+public interface Topics {
+    String EVENT_TOPIC = "tp_events";
+}
